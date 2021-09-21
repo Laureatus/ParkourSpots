aa1b20eaf0db4f75e6c5230511a46f7dcbbe3c64
Lorin Fankhauser1 <lorin.fankhauser@unic.com>
2021-09-21 10:52:16 +0200

    renamed database.html to database.php

---
+++ Website/index.php
@@ -0,0 +1,37 @@
+<?php
+    include '../Database/database.php';
+?>
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <!-- STYLESHEET -->
+    <link rel="stylesheet" type="text/css" href="src/styles.css">
+</head>
+
+<body>
+
+<!-- LEISTUNG 4 -->
+
+<section class="container" >
+    <div class="col-6">
+
+        <article class="leistungs-box leistung-box-empfohlen">
+            <h1>Parkour Spots</h1>
+            <div>
+                <?php
+
+                ?>
+            </div>
+        </article>
+
+    </div>
+
+</section>
+
+</body>
+
+</html>
